496d6853cec1d7050e83f4b4c2f4d38947404060
Jorge <newdarkjg@gmail.com>
2024-03-10 19:57:24 +0100

    Ejercicio acabado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
-import './App.css'
+import { useState } from 'react';
+
 
 function App() {
- //Aquí tu código
+    const [name, newName] = useState("Sofía");
+
+
+    let initNombres = ["Sofía", "Data", "Reyes", "Yolanda"];
+
+    const [Nombres, setNombres] = useState(initNombres)
+
+    const updateName = (nombre) => {
+
+        newName(nombre)
+    }
+    const changeName = (event) => {
+        event.preventDefault();
+        
+        if(name!="")
+            setNombres([
+                ...Nombres,
+                name
+            ])
+       
+    }
+    return (
+        <div>
+            <h2>
+                Teacher Name:{name}
+            </h2>
+            <ul>
+                {Nombres.map((Profe) => {                    
+                    return <li><button value={Profe} onClick={updateName.bind(this, Profe)}>{Profe}</button></li>
+                })}
+            </ul>
+            <form onSubmit={changeName}>
+                <label>Nuevo profesor</label>
+                <input type='text' value={name} placeholder='add a name' onChange={event => {
+                    newName(event.target.value)
+                }}>
+                </input>
+                <button type="submit" >Add</button>
+            </form>
+        </div>
+    )
 }
 
+
+
 export default App
+
